d709be8079ef82fc00cdce5943d815319b3f2624
Pham Bao <baobaoo5533@gmail.com>
2023-11-13 16:10:55 +0700

    cập nhật giao diện thông tin người dùng

---
+++ resources/views/back/pages/profile.blade.php
@@ -0,0 +1,40 @@
+@extends('back.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Hồ sơ')
+
+@section('content')
+    @livewire('author-profile-header')
+    <hr>
+    <div class="row">
+        <div class="card">
+            <div class="card-header">
+                <ul class="nav nav-tabs card-header-tabs" data-bs-toggle="tabs">
+                    <li class="nav-item">
+                        <a href="#tabs-details" class="nav-link active" data-bs-toggle="tab">Thông tin</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#tabs-password" class="nav-link" data-bs-toggle="tab">Bảo mật</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card-body">
+                <div class="tab-content">
+                    <div class="tab-pane active show" id="tabs-details">
+                        @livewire('author-personal-details')
+                    </div>
+                    <div class="tab-pane" id="tabs-password">
+                        <h4>Profile tab</h4>
+                        <div>Fringilla egestas nunc quis tellus diam rhoncus ultricies tristique enim at diam, sem nunc
+                            amet, pellentesque id egestas velit sed</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+@endsection
--- routes/author.php
+++ routes/author.php
@@ -13,5 +13,6 @@ Route::prefix('author')->name('author.')->group(function () {
     Route::middleware(['auth:web'])->group(function () {
         Route::get('/home', [AuthorController::class, 'index'])->name('home');
         Route::post('/logout', [AuthorController::class, 'logout'])->name('logout');
+        Route::view('/profile', 'back.pages.profile')->name('profile');
     });
 });
