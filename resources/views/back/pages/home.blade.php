94b2af1c4da125be416db62c26569b7b5029a538
Pham Bao <baobaoo5533@gmail.com>
2023-11-13 10:32:52 +0700

    first commit

---
+++ resources/views/back/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('back.layouts.pages-layout')
+@section('pageTitle', isset($pageTitle) ? $pageTitle : 'Trang chủ')
+@section('content')
+    xin chao content
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\AuthorController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::prefix('author')->name('author.')->group(function () {
+    Route::middleware(['guest:web'])->group(function () {
+        Route::view('/login', 'back.pages.auth.login')->name('login');
+        Route::view('/forgot-password', 'back.pages.auth.forgot')->name('forgot-password');
+    });
+
+    Route::middleware([])->group(function () {
+        Route::get('/home', [AuthorController::class, 'index'])->name('home');
+    });
+});
